d2b42875e9c72e9b3710d8abad099c133b81211a
Martin Ptáček <marty.mptacek@gmail.com>
2022-07-09 16:00:56 +0200

    RGB and HSV color representation. Fixed LED linear transition. Reindexed LED segments.Controlling backlight.

--- Software/demo_project/src/Time_o_mat/Display_TM.cpp
+++ Software/demo_project/src/Time_o_mat/Display_TM.cpp
@@ -2,34 +2,122 @@
 
 #include "Adafruit_NeoPixel.h"
 
-Adafruit_NeoPixel pixels(86, 16, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(LED_COUNT, 16, NEO_GRB + NEO_KHZ800);
 
-Color red = {255, 0, 0};
-Color green = {0, 255, 0};
-Color blue = {0, 0, 255};
-Color cyan = {0, 255, 255};
-Color magenta = {255, 0, 255};
-Color yellow = {255, 255, 0};
-Color black = {0, 0, 0};
-Color white = {255, 255, 255};
+ColorRGB black = {0, 0, 0};
+ColorRGB white = {255, 255, 255};
+ColorRGB red = {255, 0, 0};
+ColorRGB green = {0, 255, 0};
+ColorRGB blue = {0, 0, 255};
+ColorRGB cyan = {0, 255, 255};
+ColorRGB magenta = {255, 0, 255};
+ColorRGB yellow = {255, 255, 0};
+
+ColorRGB transformColorBrightness(ColorRGB color, float brightness) {
+
+    color.red = constrain(round(color.red * brightness), 0, 255);
+    color.green = constrain(round(color.green * brightness), 0, 255);
+    color.blue = constrain(round(color.blue * brightness), 0, 255);
+    return color;
+}
+
+ColorRGB HSVtoRGB(ColorHSV color) {
+    float H = constrain(color.hue, 0, 360);
+    while(H < 0) {
+        H += 360;
+    }
+    while(H > 360) {
+        H -= 360;
+    }
+    float s = constrain(color.saturation, 0, 1);
+    float v = constrain(color.value, 0, 1);
+
+    float C = s*v;
+    float X = C* (1- fabs(fmod(H/ 60, 2)- 1));
+    float m = v-C;
+    float r, g, b;
+
+    if(H > 300) {
+        r = C;
+        g = 0;
+        b = X;
+    }
+    else if(H > 240) {
+        r = X;
+        g = 0;
+        b = C;
+    }
+    else if(H > 180) {
+        r = 0;
+        g = X;
+        b = C;
+    }
+    else if(H > 120) {
+        r = 0;
+        g = C;
+        b = X;
+    }
+    else if(H > 60) {
+        r = X;
+        g = C;
+        b = 0;
+    }
+    else {
+        r = C;
+        g = X;
+        b = 0;
+    }
+    
+    ColorRGB colorRGB;
+    colorRGB.red = constrain(round((r+m)*255), 0, 255);
+    colorRGB.green = constrain(round((g+m)*255), 0, 255);
+    colorRGB.blue = constrain(round((b+m)*255), 0, 255);
+    return colorRGB;
+}
+
+ColorHSV RGBtoHSV(ColorRGB color) {
+    // TODO
+    ColorHSV colorHSV;
+    return colorHSV;
+}
 
 void Display_TM::begin() {
     pixels.begin();
 }
 
 void Display_TM::update() {
-    static uint32_t prevTime = millis();
-    for(uint8_t ledID = 0; ledID < 86; ++ledID) {
+    static int prevTime = millis();
+    for(uint8_t ledID = 0; ledID < LED_COUNT; ++ledID) {
         switch(transitionType) {
             case Linear: {
-                float deviation = desiredState[ledID].red - currentState[ledID][0];
+                float deviation;
                 float step = transitionRate * (millis() - prevTime) / 1000.0 * 255.0;
+
+                deviation = desiredState[ledID].red - currentState[ledID][0];
                 if(abs(deviation) < (step + 1.0)) {
                     currentState[ledID][0] = desiredState[ledID].red;
                 }
                 else {
                     int8_t sign = deviation > 0 ? 1 : -1;
                     currentState[ledID][0] += sign * step;
+                }
+
+                deviation = desiredState[ledID].green - currentState[ledID][1];
+                if(abs(deviation) < (step + 1.0)) {
+                    currentState[ledID][1] = desiredState[ledID].green;
+                }
+                else {
+                    int8_t sign = deviation > 0 ? 1 : -1;
+                    currentState[ledID][1] += sign * step;
+                }
+
+                deviation = desiredState[ledID].blue - currentState[ledID][2];
+                if(abs(deviation) < (step + 1.0)) {
+                    currentState[ledID][2] = desiredState[ledID].blue;
+                }
+                else {
+                    int8_t sign = deviation > 0 ? 1 : -1;
+                    currentState[ledID][2] += sign * step;
                 }
             } break;
             case InfiniteImpulseResponse:
@@ -44,66 +132,110 @@ void Display_TM::update() {
     }
     pixels.show();
     prevTime = millis();
-    printf("%f\n", currentState[0][0]);
 }
 
-void Display_TM::setLED(const int16_t digitIndex, const int16_t position, const Color color) {
-    if(digitIndex < 0 || digitIndex > 4)
+void Display_TM::setLED(int segmentID, int ledID, ColorRGB color) {
+    if(segmentID < 0 || segmentID > 5)
         return;
-    if(position < 0 || position > 20)
+    if(ledID < 0 || ledID > 20)
         return;
     
-    int16_t ledID = 0;
-    if(digitIndex < 2)
-        ledID = digitIndex*21 + charToIndexMap[position];
-    else if(digitIndex < 4)
-        ledID = digitIndex*21 + charToIndexMap[position] + 2;   // center colon offset
-    else
-        ledID = position + 42;
+    int ledAbsoluteID = 0;
+    int ledSide = 0;    // 0-front, 1-back
 
-    if(ledID < 0 || ledID > 85) {
+    // 2 leftmost digits
+    if(segmentID <= 1)
+        ledAbsoluteID = segmentID * 21 + charToIndexMap[ledID];
+
+    // Center colon
+    else if(segmentID == 2) {
+        if(ledID > 1)
+            return;
+        ledAbsoluteID = segmentID * 21 + ledID;
+    }
+        
+    // 2 rightmost digits
+    else if(segmentID <= 4)
+        ledAbsoluteID = (segmentID - 1) * 21 + 2 + charToIndexMap[ledID];
+    
+    // Backlight
+    else {
+        if(ledID > 8)
+            return;
+        ledAbsoluteID = (segmentID - 1) * 21 + 2 + ledID;
+        ledSide = 1;
+    }
+
+    if(ledAbsoluteID < 0 || ledAbsoluteID > LED_COUNT) {
         return;
     }
     
-    desiredState[ledID] = transformColorBrightness(color, brightness);
+    desiredState[ledAbsoluteID] = transformColorBrightness(color, panelBrightness[ledSide]);
+}
+void Display_TM::setLED(int digitIndex, int ledID, ColorHSV color) {
+    ColorRGB colorRGB = HSVtoRGB(color);
+    setLED(digitIndex, ledID, colorRGB);
 }
 
-void Display_TM::setChar(const int16_t digitIndex, const char character, const Color color) {
-    if(digitIndex < 0 || digitIndex > 3)
+void Display_TM::setChar(int charID, char character, ColorRGB color) {
+    if(charID < 0 || charID > 3)
         return;
     if(character < 40 || character > 91)
         return;
 
+    int segmentID = charID;
+    if(segmentID >= 2)
+        ++segmentID;    // center colon segment offset
     for(uint8_t i = 0; i < 21; ++i) {
         if(characterSet[character - 40][i]) {
-            setLED(digitIndex, i, color);
+            setLED(segmentID, i, color);
         }
         else {
-            setLED(digitIndex, i, black);
+            setLED(segmentID, i, black);
         }
     }
 }
-
-void Display_TM::setColon(const Color colorTop, const Color colorBottom) {
-    setLED(4, 1, colorTop);
-    setLED(4, 0, colorBottom);
+void Display_TM::setChar(int charID, char character, ColorHSV color) {
+    ColorRGB colorRGB = HSVtoRGB(color);
+    setChar(charID, character, colorRGB);
 }
 
-void Display_TM::setText(const String text, const Color color) {
+void Display_TM::setColon(ColorRGB color) {
+    setLED(2, 0, color);
+    setLED(2, 1, color);
+}
+void Display_TM::setColon(ColorHSV color) {
+    ColorRGB colorRGB = HSVtoRGB(color);
+    setColon(colorRGB);
+}
+
+void Display_TM::setText(String text, ColorRGB color) {
     for(uint8_t i = 0; i < 4; ++i) {
         setChar(i, text[i], color);
     }
 }
-
-Color Display_TM::transformColorBrightness(Color color, int16_t brightness) {
-    color.red = constrain(round(color.red * brightness / 255.0), 0, 255);
-    color.green = constrain(round(color.green * brightness / 255.0), 0, 255);
-    color.blue = constrain(round(color.blue * brightness / 255.0), 0, 255);
-    return color;
+void Display_TM::setText(String text, ColorHSV color) {
+    ColorRGB colorRGB = HSVtoRGB(color);
+    setText(text, colorRGB);
 }
 
-void Display_TM::setBrightness(int16_t aBrightness) {
-    brightness = constrain(aBrightness, 0, 255);
+void Display_TM::setBacklight(ColorRGB color) {
+    for(int i = 0; i < 9; ++i) {
+        setLED(5, i, color);
+    }
+}
+
+void Display_TM::setBacklight(ColorHSV color) {
+    ColorRGB colorRGB = HSVtoRGB(color);
+    setBacklight(colorRGB);
+}
+
+void Display_TM::setBrightnessFront(float brightness) {
+    panelBrightness[0] = constrain(brightness, 0.0, 1.0);
+}
+
+void Display_TM::setBrightnessBack(float brightness) {
+    panelBrightness[1] = constrain(brightness, 0.0, 1.0);
 }
 
 void Display_TM::setTransition(TransitionType aTransitionType, float aTransitionRate) {
@@ -111,9 +243,9 @@ void Display_TM::setTransition(TransitionType aTransitionType, float aTransition
     transitionRate = aTransitionRate;
 }
 
-float Display_TM::currentState[86][3] = {0.0, };
-Color Display_TM::desiredState[86] = {black, };
-int16_t Display_TM::brightness = 255;
+float Display_TM::currentState[LED_COUNT][3] = {0.0, };
+ColorRGB Display_TM::desiredState[LED_COUNT] = {black, };
+float Display_TM::panelBrightness[] = {1.0, 1.0};
 TransitionType Display_TM::transitionType = None;
 float Display_TM::transitionRate = 1.0;
 uint8_t Display_TM::charToIndexMap[21] = {0, 1, 2, 17, 3, 16, 4, 15, 5, 18, 19, 20, 14, 6, 13, 7, 12, 8, 11, 10, 9};
--- Software/demo_project/src/Time_o_mat/Display_TM.h
+++ Software/demo_project/src/Time_o_mat/Display_TM.h
@@ -3,11 +3,23 @@
 
 #include "Arduino.h"
 
-typedef struct color {
+#define LED_COUNT 95
+
+typedef struct colorRGB {
     uint8_t red;
     uint8_t green;
     uint8_t blue;
-} Color;
+} ColorRGB;
+
+typedef struct colorHSV {
+    float hue;
+    float saturation;
+    float value;
+} ColorHSV;
+
+ColorRGB transformColorBrightness(ColorRGB color, float brightness);
+ColorRGB HSVtoRGB(ColorHSV color);
+ColorHSV RGBtoHSV(ColorRGB color);
 
 enum TransitionType {
     None = 0,
@@ -15,34 +27,45 @@ enum TransitionType {
     InfiniteImpulseResponse = 2
 };
 
-extern Color red;
-extern Color green;
-extern Color blue;
-extern Color cyan;
-extern Color magenta;
-extern Color yellow;
-extern Color black;
-extern Color white;
+extern ColorRGB red;
+extern ColorRGB green;
+extern ColorRGB blue;
+extern ColorRGB cyan;
+extern ColorRGB magenta;
+extern ColorRGB yellow;
+extern ColorRGB black;
+extern ColorRGB white;
 
 class Display_TM {
     static uint8_t charToIndexMap[21];
     static bool characterSet[51][21];
-    static float currentState[86][3];
-    static Color desiredState[86];
-    static int16_t brightness;
+    static float currentState[LED_COUNT][3];    // {red, green, blue}
+    static ColorRGB desiredState[LED_COUNT];
+    static float panelBrightness[2];                 // {front, back}
     static TransitionType transitionType;
     static float transitionRate;
 
 public:
     static void begin();
     static void update();
-    static void setLED(const int16_t digitIndex, const int16_t position, const Color color);
-    static void setChar(const int16_t digitIndex, const char character, const Color color);
-    static void setColon(const Color colorTop, const Color colorBottom);
-    static void setText(const String text, const Color color);
 
-    static Color transformColorBrightness(Color color, int16_t brightness);
-    static void setBrightness(int16_t aBrightness);
+    static void setLED(int segmentID, int ledID, ColorRGB color);
+    static void setLED(int segmentID, int ledID, ColorHSV color);
+
+    static void setChar(int charID, char character, ColorRGB color);
+    static void setChar(int charID, char character, ColorHSV color);
+
+    static void setColon(ColorRGB color);
+    static void setColon(ColorHSV color);
+
+    static void setText(String text, ColorRGB color);
+    static void setText(String text, ColorHSV color);
+
+    static void setBacklight(ColorRGB color);
+    static void setBacklight(ColorHSV color);
+
+    static void setBrightnessFront(float brightness);
+    static void setBrightnessBack(float brightness);
     static void setTransition(TransitionType aTransitionType, float aTransitionRate);
 };
 
