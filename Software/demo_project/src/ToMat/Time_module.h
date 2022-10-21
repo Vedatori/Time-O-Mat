36df578eb637bc3fae49f0a22670f43fe4a8e9f4
Martin Ptáček <marty.mptacek@gmail.com>
2022-10-21 18:47:07 +0200

    Revert "Time module rework"
    This reverts commit 9a64cf988bfc1bbdb4341091c141d592ec8ff98f.

--- Software/demo_project/src/ToMat/Time_module.h
+++ Software/demo_project/src/ToMat/Time_module.h
@@ -4,52 +4,27 @@
 #include "Arduino.h"
 #include "sntp.h"
 #include "time.h"
-#include "Preferences.h"
 
 enum TimeSource {
-	TS_Internet,
-	TS_RTC,
- 	TS_Manual
+    TimeInternet,
+    TimeRTC,
+    TimeManual
 };
-
-
-
 
 const char ntpServer1[] = "pool.ntp.org";
 const char ntpServer2[] = "time.nist.gov";
+const long gmtOffset_sec = 3600;
+const int daylightOffset_sec = 3600;
 
 //const char time_zone[] = "CET-1CEST,M3.5.0,M10.5.0/3";  // TimeZone rule for Europe/Rome including daylight adjustment rules (optional)
 
 class Time_module {
-    time_t epochTime = 0; //Epoch starting on 1. January 1970
-	int zoneOffset = 0; //seconds
-	int daylightOffset = 0; //seconds
-	bool offline = false;
-	TimeSource timeSource = TS_RTC;
-	uint32_t rtcUpdatePeriod = 200; //ms
-	Preferences preferences;
+    struct tm time;
+	const uint32_t rtcGetTimeDelay = 300;
 public:
     void begin();
-
-	bool updateNtp();
-	bool updateRtc(bool forced = false);
-
-    tm getLocalTime();
-	tm getGmtTime();
-	time_t getEpoch();
-	String getLocalTimeStamp();
-	String getGmtTimeStamp();
-	
-	void setTimeZone(int zoneOffset_sec, int daylightOffset_sec, bool saveToPreferences);
-	int getTimeZoneOffset();
-	int getDaylightOffset();
-
-	void setOfflineMode(bool isOffline, bool saveToPreferences);
-	bool isOffline();
-	bool setOfflineLocalTime(tm newTime);
-
-	
-
+	bool updateTime();
+    struct tm getTime();
     TimeSource getTimeSource();
     String getClockText();
 };
